7280e87451cffcf0d776f53f5fcb0e0cb6978ced
lujian <lujian@yzu.edu.cn>
2023-02-09 17:57:50 +0800

    Update Eapp.py

--- Eapp.py
+++ Eapp.py
@@ -7,7 +7,7 @@ st.slider("大小", min_value=10, max_value=100, value=20, step=10)
 st.selectbox("方向", options=["正向", "反向"])
 
 # 生成E的图片
-img = "<style>img{width:100px; height:100px;}</style><img src='https://via.placeholder.com/100x100.png'>"
+img = "<style>img{width:100px; height:100px;}</style><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/LetterE.svg/800px-LetterE.svg.png'>"
 
 # 将E的图片显示在streamlit上
 st.write(img, unsafe_allow_html=True)
