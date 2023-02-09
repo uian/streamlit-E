0b827438c7c613f5f54dc95b8419ad7e349338ff
lujian <lujian@yzu.edu.cn>
2023-02-09 10:05:30 +0000

    unsafe_allow_html=True

--- app.py
+++ app.py
@@ -8,18 +8,18 @@ def main():
 
     if orientation == 'Horizontal':
         if color == 'Red':
-            st.write("<span style='color:red; font-size:{}px'>E</span>".format(size))
+            st.write("<span style='color:red; font-size:{}px'>E</span>".format(size),unsafe_allow_html=True)
         elif color == 'Green':
-            st.write("<span style='color:green; font-size:{}px'>E</span>".format(size))
+            st.write("<span style='color:green; font-size:{}px'>E</span>".format(size),unsafe_allow_html=True)
         elif color == 'Blue':
-            st.write("<span style='color:blue; font-size:{}px'>E</span>".format(size))
+            st.write("<span style='color:blue; font-size:{}px'>E</span>".format(size),unsafe_allow_html=True)
     elif orientation == 'Vertical':
         if color == 'Red':
-            st.write("<div style='writing-mode: vertical-lr; color:red; font-size:{}px'>E</div>".format(size))
+            st.write("<div style='writing-mode: vertical-lr; color:red; font-size:{}px'>E</div>".format(size),unsafe_allow_html=True)
         elif color == 'Green':
-            st.write("<div style='writing-mode: vertical-lr; color:green; font-size:{}px'>E</div>".format(size))
+            st.write("<div style='writing-mode: vertical-lr; color:green; font-size:{}px'>E</div>".format(size),unsafe_allow_html=True)
         elif color == 'Blue':
-            st.write("<div style='writing-mode: vertical-lr; color:blue; font-size:{}px'>E</div>".format(size))
+            st.write("<div style='writing-mode: vertical-lr; color:blue; font-size:{}px'>E</div>".format(size),unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
